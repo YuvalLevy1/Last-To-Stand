11c975f9e006fe0390d726b2e8c304ab8508fef4
YuvalLevy1 <yuval124levy1@gmail.com>
2020-05-18 13:54:49 +0300

    added a method that updates the game using data from tcp

--- src/game/game_client.py
+++ src/game/game_client.py
@@ -1,19 +1,18 @@
-import pygame
-
+import ast
 import os
 
+import pygame
+
 from game import game_map
-from network import tcp_client
+from network import tcp_client, udp_client, network_player, network_constants
 
 
 class Game:
-
     window_height = 1080
     window_width = 1920
 
     def __init__(self):
         self.screen = None
-        self.player = None  # the main player
         self.opponents = None  # his online opponents
         self.players = None  # all players, the main player and his opponents
         self.running = False  # whether the game is running or not
@@ -25,16 +24,11 @@ class Game:
         # the screen's corners
         self.clock = pygame.time.Clock()  # used to set the game ticks per second
         self.tcp_client = tcp_client.TCPClient()
+        self.player_id = None
+        self.udp_client = None
 
     def draw_game_window(self):
-        self.screen.blit(self.background, (0, 0))  # the method responsible for drawing on the screen.
-
-    def get_all_living_players(self):
-        players = [self.player]
-        for opponent in self.opponents:
-            if not opponent.is_dead:
-                players.append(opponent)
-        return players
+        self.screen.blit(self.background, (0, 0))
 
     def game_over(self):  # detects whether the player is dead and closes the game.
         self.running = False
@@ -52,7 +46,61 @@ class Game:
         self.screen = pygame.display.set_mode((Game.window_width, Game.window_height))
         pygame.display.set_caption("Last To Stand")
         pygame.init()
-        return self.connect_to_server()
+        data = self.connect_to_server()
+        self.player_id = self.receive_tcp_from_server()
+        return data
 
     def receive_tcp_from_server(self):
         return self.tcp_client.receive()
+
+    def by_event(self, event):
+        """ callback after events """
+        if event.type == event.QUIT:
+            if self.players[self.player_id].udp_client:
+                self.players[self.player_id].udp_client.send("Bye")
+            self.running = False
+
+    def tcp_update(self, data):
+        data = (data.decode()).strip()
+
+        if data.startswith("We need to wait to"):  # we got "Wait to start:0" or "Wait to start:1"
+            print(data.decode())
+
+        elif data.startswith("id"):
+            self.player_id = int(data.split(":")[1])
+
+        elif data == "starting":
+            self.udp_client = udp_client.UDPClient(self.player_id)
+            spawn_points = game_map.spawn_points
+            player = network_player.NetworkPlayer(spawn_points[self.player_id][0], spawn_points[self.player_id][1],
+                                                  self.BORDERS, self.player_id)
+            self.players[self.player_id] = player
+        elif data == "Bye":
+            self.running = False
+            self.udp_client.disconnect()
+        elif data.startswith("update"):
+            data = data.split(":")[1]
+            data = eval(data)
+            for i in range(len(self.players)):
+                self.players[i].use_info_tcp(data[i])
+
+    def init_players(self):
+        spawn_points = game_map.spawn_points
+        for i in range(network_constants.MAX_NUM_OF_CLIENTS):
+            if i != self.player_id:
+                enemy = network_player.NetworkPlayer(spawn_points[i][0], spawn_points[i][1],
+                                                     self.BORDERS, i)
+                self.players[i] = enemy
+
+    def execute(self):
+        self.init_game()
+
+        self.by_event(pygame.event.get())
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()

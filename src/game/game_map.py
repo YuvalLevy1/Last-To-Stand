20855a24af963e1cdffa4921261c402069121536
YuvalLevy1 <yuval124levy1@gmail.com>
2020-05-20 11:20:39 +0300

    made the background's path absolute and added constants for the map's limits

--- src/game/game_map.py
+++ src/game/game_map.py
@@ -1,5 +1,9 @@
 import pygame
 
-map_background = pygame.image.load("images\\background.png")
+map_background = pygame.image.load("D:\\programming\\Git\\Network-Game\\src\\game\\images\\background.png")
 
 spawn_points = [(105, 84), (1729, 84), (1729, 882), (119, 882)]
+
+BORDERS_TOP_LEFT = (0, 0)
+BORDERS_BOTTOM_RIGHT = (1920, 1080)
+BORDERS = (BORDERS_TOP_LEFT, BORDERS_BOTTOM_RIGHT)
